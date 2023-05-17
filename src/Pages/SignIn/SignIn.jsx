c5fb957c1df15e43f55c75e702f6b91e83a4bebf
lJayCoding <lukewebber392@gmail.com>
2023-05-17 09:23:40 +0200

    login

---
+++ src/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import Login from '../../components/Login/Login';
+import NavBar from '../../components/NavBar/NavBar';
+
+const SignIn = () => {
+  return (
+    <Box>
+      <NavBar />
+      <Login />
+    </Box>
+  );
+};
+export default SignIn;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
-import { GoogleLogin, googleLogout } from "@react-oauth/google";
-import jwt_decode from "jwt-decode";
-import useLocalStorage from "use-local-storage";
+import { Box, Button, Typography } from '@mui/material';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import useLocalStorage from 'use-local-storage';
 
 function Login() {
-  const [user, setUser] = useLocalStorage("user", null);
+  const [user, setUser] = useLocalStorage('user', null);
 
   const responseMessage = (response) => {
     setUser(jwt_decode(response.credential));
@@ -16,20 +16,56 @@ function Login() {
 
   const logOut = () => {
     googleLogout();
-
     setUser(null);
   };
 
   return (
     <>
-      {!user ? (
-        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-      ) : (
-        <>
-          <span>{user?.email}</span>
-          <Button onClick={logOut}>Log out</Button>
-        </>
-      )}
+      {' '}
+      <Box
+        sx={{
+          width: '210px',
+          margin: 'auto',
+          top: '5vh',
+          position: 'relative',
+        }}
+      >
+        {!user ? (
+          <GoogleLogin
+            onSuccess={responseMessage}
+            onError={errorMessage}
+            size="medium"
+            shape="pill"
+            theme="filled_blue"
+            text="signin_with"
+          />
+        ) : (
+          <>
+            <Button
+              onClick={logOut}
+              sx={{
+                position: 'relative',
+                left: '55px',
+                '&:hover': {
+                  fontWeight: 'bold',
+                },
+              }}
+              variant="contained"
+            >
+              Log out
+            </Button>
+            <Typography
+              sx={{
+                fontWeight: 'bold',
+                textAlign: 'center',
+                margin: '10px',
+              }}
+            >
+              Welcome, {user.given_name}.
+            </Typography>
+          </>
+        )}{' '}
+      </Box>
     </>
   );
 }
